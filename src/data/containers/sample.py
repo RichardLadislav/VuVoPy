4445a17ccb6df14c82c1d1bc660436398f94e8b7
RiskoLadislav <richard.ladislav@gmail.com>
2025-03-19 17:21:22 +0100

    VoiceSample fixed

--- src/data/containers/sample.py
+++ src/data/containers/sample.py
@@ -1,17 +1,24 @@
 import librosa
+import numpy as np
 
+class VoiceSample:
+    """Class to load and process audio samples."""
 
-class VoiceSample(object):
-    """Creating sample class from audio"""
-
-    def __init__(self, x, fs):
-        """Constructor method"""
-        self.x = x 
+    def __init__(self, x: np.ndarray, fs: int):
+        """Initialize with audio waveform and sampling rate."""
+        self.x = x
         self.fs = fs
-        
-        
-    @classmethod
-    def from_wav(file_path, sr=None):
-        "Initializes VoiceSample object from wav file"
-        return librosa.load(file_path, sr) 
 
+    @classmethod
+    def from_wav(cls, file_path: str, sr: int = None):
+        """Load a WAV file and return a VoiceSample instance."""
+        x, fs = librosa.load(file_path, sr=sr)
+        return cls(x, fs)
+
+    def get_waveform(self):
+        """Return the waveform as a NumPy array."""
+        return self.x
+
+    def get_sampling_rate(self):
+        """Return the sampling rate."""
+        return self.fs
