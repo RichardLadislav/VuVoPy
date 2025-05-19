da9e779f754afa45cf1d79031de9ce0b95a4d11b
RiskoLadislav <richard.ladislav@gmail.com>
2025-05-19 17:29:59 +0200

    99.9 p documentation

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ release = '0.0.2a'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode", "sphinx.ext.autosummary"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
